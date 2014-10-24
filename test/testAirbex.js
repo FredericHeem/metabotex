d6ab70171cdebeea0f79805ccabce4547fbf9854
Frederic Heem <frederic.heem@gmail.com>
2014-10-24 12:35:22 +0200

    wrap Airbex like others exchange

---
+++ test/testAirbex.js
@@ -0,0 +1,35 @@
+/*global describe, it, before, after*/
+var assert = require('assert');
+var request = require('supertest');
+var async = require('async');
+var _ = require('underscore');
+var config = require('./configTest.js')();
+var debug = require('debug')('Airbex');
+var AirbexEx = require('../lib/Airbex');
+var num = require('num');
+
+describe('Airbex', function () {
+    "use strict";
+    var api;
+    
+    describe('AirbexRest', function () {
+        before(function(done) {
+            api = new AirbexEx.RestClient(config.target);
+            api.ee().on('error', function(error){
+                assert(error)
+                done(error);
+            });
+            done()
+        });
+        it('AirbexBalance', function (done) {
+            api.getBalances()
+            .then(function(balances){
+                assert(balances)
+                assert(balances['BTC'])
+                done();
+            })
+            .fail(done)
+        });
+        
+    });
+});
