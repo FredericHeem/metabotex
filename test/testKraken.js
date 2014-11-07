84afd86a8304578450e423fcfa9aa5a2a4accfd6
Frederic Heem <frederic.heem@gmail.com>
2014-11-07 11:11:30 +0100

    add kraken

---
+++ test/testKraken.js
@@ -0,0 +1,63 @@
+/*global describe, it, before, after*/
+var assert = require('assert');
+var request = require('supertest');
+var async = require('async');
+var _ = require('underscore');
+var config = require('./configTest.js')('krakenbtceurlocal');
+var debug = require('debug')('Kraken');
+var KrakenEx = require('../lib/Kraken');
+var num = require('num');
+
+describe('Kraken', function () {
+    "use strict";
+    var api;
+    
+    describe('KrakenRest', function () {
+        before(function(done) {
+            api = new KrakenEx.RestClient(config.provider);
+            api.ee().on('error', function(error){
+                assert(error)
+                done(error);
+            });
+            done()
+        });
+        it('KrakenBalance', function (done) {
+            this.timeout(5e3)
+            api.getBalances()
+            .then(function(balances){
+                assert(balances)
+                assert(balances['BTC'])
+                done();
+            })
+            .fail(done)
+        });
+        it('KrakenDepth', function (done) {
+            this.timeout(5e3)
+            api.getDepth("BTCEUR")
+            .then(function(depth){
+                assert(depth)
+                assert(depth.bids[0])
+                assert(depth.asks[0])
+                var bid = depth.bids[0] 
+                done();
+            })
+            .fail(done)
+        });
+        it('KrakenMonitorOrderBookOk', function (done) {
+            this.timeout(20e3);
+            var numBook = 0;
+            api.ee().on('orderBook', function(depth){
+                assert(depth)
+                assert(depth.bids)
+                assert(depth.asks);
+                numBook++;
+                if(numBook === 3){
+                    done();
+                }
+            });
+
+            api.monitorOrderBook("BTCEUR");
+        });
+        
+    });
+});
